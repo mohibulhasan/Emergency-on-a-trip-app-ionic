82525137b7862da81f5be2d094f402535707f7e6
mohibulhasan <mohibul-hasan@outlook.com>
2025-07-23 16:51:22 +0100

    Installed the environment

---
+++ journeyTracker/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.md.journeyTracker',
+  appName: 'journeyTracker',
+  webDir: 'www'
+};
+
+export default config;
